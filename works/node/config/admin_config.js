f81daa5a4225e25d9af916280d9ca79665a372a6
chngoshawk <mrzousheng@hotmail.com>
2018-05-31 22:34:54 +0800

    update

--- works/node/config/admin_config.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/admin_data',function(err){
-    if(err){
-        console.log(err);
-    }
-});
-
-module.exports = mongoose;
