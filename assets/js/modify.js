6e69a5a93a0101e36f5c54419e9e82e420f2c22d
chngoshawk1 <40968799+chngoshawk1@users.noreply.github.com>
2018-09-07 14:26:38 +0800

    update
    update

--- assets/js/modify.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $('.projects>ul').hide();
-    $('.projects>li').on('click',function(){
-        $('.projects>ul').hide(500);
-        $(this).next('ul').is(':visible') || $(this).next('ul').show(500);
-    });
-});
